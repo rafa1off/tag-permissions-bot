8645430baa909052d4b2b86cd9ac776b01a90b07
Rafael Giuriatto <rafagiuriatto@hotmail.com>
2023-05-03 22:00:34 -0300

    add & remove

--- bot.py
+++ bot.py
@@ -12,11 +12,27 @@ client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.command(name='add')
+@commands.has_permissions(administrator=True)
 async def add(ctx, *users: discord.Member):
-    if discord.utils.get(ctx.guild.roles, name='Gazebos') in ctx.author.roles:
-        for user in users:
-            await ctx.send(user.roles)
-    else:
+    for user in users:
+        channel = bot.get_channel(ctx.channel.id)
+        await channel.set_permissions(user, view_channel=True, send_messages=True, add_reactions=True)
+
+@add.error
+async def add_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send('Not enough permissions')
+
+@bot.command(name='remove')
+@commands.has_permissions(administrator=True)
+async def remove(ctx, *users: discord.Member):
+    for user in users:
+        channel = bot.get_channel(ctx.channel.id)
+        await channel.set_permissions(user, overwrite=None)
+
+@remove.error
+async def remove_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
         await ctx.send('Not enough permissions')
 
 bot.run(token)
