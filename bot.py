00d9e856c44cdaea5d5c0b9e4644968bc7d10a00
Rafael Giuriatto <rafagiuriatto@hotmail.com>
2023-05-04 19:45:05 -0300

    conditions

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from quotes import Quotes as quote
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -11,28 +12,51 @@ intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-@bot.command(name='add', help='Add user with permissions to view channel, send messages and add reactions')
+
+@bot.command(name='add', help=quote.add_command)
 @commands.has_permissions(administrator=True)
 async def add(ctx, *users: discord.Member):
-    for user in users:
-        channel = bot.get_channel(ctx.channel.id)
-        await channel.set_permissions(user, view_channel=True, send_messages=True, add_reactions=True)
+    if users:
+        for user in users:
+            channel = bot.get_channel(ctx.channel.id)
+            await channel.set_permissions(user, view_channel=True, send_messages=True, add_reactions=True)
+    else:
+        await ctx.send(quote.specify_user)
+
 
 @add.error
 async def add_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
-        await ctx.send('Not enough permissions')
+        await ctx.send(quote.missing_permissions)
+    elif isinstance(error, commands.MemberNotFound):
+        await ctx.send(quote.member_not_found)
 
-@bot.command(name='remove', help='Remove added users')
+
+@bot.command(name='remove', help=quote.remove_command)
 @commands.has_permissions(administrator=True)
 async def remove(ctx, *users: discord.Member):
-    for user in users:
+    if users:
+        for user in users:
+            channel = bot.get_channel(ctx.channel.id)
+            await channel.set_permissions(user, overwrite=None)
+    else:
         channel = bot.get_channel(ctx.channel.id)
-        await channel.set_permissions(user, overwrite=None)
+        for member in channel.members:
+            await channel.set_permissions(member, overwrite=None)
+
 
 @remove.error
 async def remove_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
-        await ctx.send('Not enough permissions')
+        await ctx.send(quote.missing_permissions)
+    elif isinstance(error, commands.MemberNotFound):
+        await ctx.send(quote.member_not_found)
+
+
+@bot.command(name='members')
+@commands.is_owner()
+async def members(ctx, *users: discord.Member):
+    pass
+
 
 bot.run(token)
---
+++ quotes.py
@@ -0,0 +1,6 @@
+class Quotes:
+    add_command = 'Add user with permissions to view channel, send messages and add reactions'
+    remove_command = 'Remove added users'
+    specify_user = 'Specify users: $add @users'
+    missing_permissions = 'Not enough permissions'
+    member_not_found = 'Members not found'
